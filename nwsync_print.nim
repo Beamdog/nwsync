7a50122fd0e2044acd6dfcea17e5a3b25249f854
niv <n@e-ix.net>
2021-06-11 10:58:51 +0200

    nwn_print --verify checkums manifest contents

--- nwsync_print.nim
+++ nwsync_print.nim
@@ -13,17 +13,21 @@ Options:
   -V --version      Show version.
   -v --verbose      Verbose operation (>= DEBUG).
   -q --quiet        Quiet operation (>= WARN).
+
+  --verify          Verify presence and checksum of files in manifest.
 """
 
 from libversion import handleVersion
 if ARGS["--version"]: handleVersion()
 
-import logging, streams, strutils, options, os
+import logging, streams, strutils, options, os, std/sha1
 
-import libmanifest, neverwinter/resref
+import libmanifest, libupdate, neverwinter/compressedbuf, neverwinter/resref
 
 addHandler newFileLogger(stderr, fmtStr = verboseFmtStr)
 setLogFilter(if ARGS["--verbose"]: lvlDebug elif ARGS["--quiet"]: lvlWarn else: lvlInfo)
+
+let doVerify = ARGS["--verify"]
 
 doAssert(fileExists($ARGS["<manifest>"]))
 let mf = readManifest(newFileStream($ARGS["<manifest>"], fmRead))
@@ -53,3 +57,10 @@ for entry in mf.entries:
     align(rystr, 7),
     " ",
     escape(entry.resref.resref, "", "")
+
+  if doVerify:
+    let pa = pathForEntry(mf, $ARGS["<manifest>"] / ".." / "..", entry.sha1, false)
+    let fs = openFileStream(pa, fmRead)
+    defer: fs.close()
+    if entry.sha1 != toLowerAscii $secureHash(decompress(fs, makeMagic("NSYC"))):
+      raise newException(ValueError, "File checksum mismatch: " & pa)
